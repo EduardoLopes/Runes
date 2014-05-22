8575cb245f01c5afab9d58a28755851d35620cce
Eduardo Lopes <EduardoLopesDS@gmail.com>
2014-05-22 16:21:37 -0300

    Some optimizations

--- runes.js
+++ runes.js
@@ -56,7 +56,7 @@
       currentLetter;
 
   /**
-  * Handle a text
+  * Handle the text
   * @author    Eduardo Lopes (EduardoLopesDS@gmail.com)
   * @github    https://github.com/EduardoLopes/Runes
   * @licence   https://github.com/EduardoLopes/Runes/blob/master/LICENSE
@@ -71,29 +71,48 @@
     this.letterIndex = 0;
     this.x = options.x;
     this.y = options.y;
+    this.preCanvas = document.createElement('canvas');
+    this.preContext = this.preCanvas.getContext('2d');
+    this.done = false;
+    this.containerWidth = options.containerWidth;
+    this.preCanvas.width = this.containerWidth;
   }
 
   Runes.prototype.render = function() {
     this.letterIndex = 0;
-    for(letters in this.text){
-      currentLetter = this.text[letters];
-      if(typeof this.font[currentLetter] !== 'undefined'){
-        for (var h = this.font[currentLetter].length - 1; h >= 0; h--) {
-          for (var w = this.font[currentLetter][h].length - 1; w >= 0; w--) {
-            if(this.font[currentLetter][h][w]){
-              this.context.fillRect(Math.round(this.x + (w * this.size) + (this.letterIndex * (this.size * (this.font[currentLetter].length + 1)))), Math.round(this.y + (h * this.size)), this.size, this.size);
+    if(this.done === false){
+      this.preContext.clearRect(0,0,this.preCanvas.width, this.preCanvas.height);
+      for(letters in this.text){
+        currentLetter = this.text[letters];
+        if(typeof this.font[currentLetter] !== 'undefined'){
+          for (var h = this.font[currentLetter].length - 1; h >= 0; h--) {
+            for (var w = this.font[currentLetter][h].length - 1; w >= 0; w--) {
+              if(this.font[currentLetter][h][w]){
+                this.preContext.fillRect(Math.round((w * this.size) + (this.letterIndex * (this.size * (this.font[currentLetter].length + 1)))), Math.round((h * this.size)), this.size, this.size);
+              }
             }
           }
         }
+        this.letterIndex++;
       }
-      this.letterIndex++;
     }
+
+    this.done = true;
+
+    this.context.drawImage(this.preCanvas, this.x, this.y);
   };
 
   Runes.prototype.setText = function (text) {
     this.text = text;
+    this.done = false;
 
     return text;
+  };
+
+  Runes.prototype.setSize = function (size) {
+    this.size = Math.round(size);
+    this.done = false;
+    return size;
   };
 
   Runes.prototype.setPosition = function(x, y) {
